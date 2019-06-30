2636d81876d43ba0154b455d647d84966a4a6037
TadeuszNorek <226098@student.pwr.edu.pl>
2019-06-30 15:56:49 +0200

    Task9

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -61,4 +61,9 @@ public class StringCalculatorTest {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(6, StringCalculator.add("//[*][%]\n1*2%3"));
     }
+    @Test
+    public void multipleAnyLengthDelimetersTest() {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(6, StringCalculator.add("//[****][%%]\n1****2%%3"));
+    }
 }
